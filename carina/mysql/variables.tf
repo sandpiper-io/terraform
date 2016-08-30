99c9da4af88788b351801fb46f9fd2af6aec997a
Erik Dahl <lslatr@gmail.com>
2016-08-30 12:45:24 -0400

    Initial Terraform module for Carina MySQL example

---
+++ carina/mysql/variables.tf
@@ -0,0 +1,18 @@
+variable "mysql_root_password" {
+  default = "root-password"
+}
+variable "mysql_database" {
+  default = "guestbook"
+}
+variable "mysql_user" {
+  default = "guestbook-user"
+}
+variable "mysql_password" {
+  default = "guestbook-user-password"
+}
+variable "mysql_host" {
+  default = "mysql"
+}
+variable "mysql_port" {
+  default = "3306"
+}
